b2f49c46e13d9ad8fbf7cbc8ccb04d2f0503b280
DevRed91 <51876334+DevRed91@users.noreply.github.com>
2020-05-22 10:45:46 +0530

    Update SelectionButton.js

--- public/js/extensions/SelectionButton.js
+++ public/js/extensions/SelectionButton.js
@@ -3,6 +3,7 @@ class HandleSelectionExtension extends Autodesk.Viewing.Extension {
         super(viewer, options);
         this._group = null;
         this._button = null;
+        this._modelData = {};
     }
 
     load() {
@@ -35,6 +36,20 @@ class HandleSelectionExtension extends Autodesk.Viewing.Extension {
 
     }
 
+    getAllLeafComponents(callback) {
+        this.viewer.getObjectTree(function (tree) {
+            let leaves = [];
+            tree.enumNodeChildren(tree.getRootId(), function (dbId) {
+                if (tree.getChildCount(dbId) === 0) {
+                    leaves.push(dbId);
+                }
+            }, true);
+            callback(leaves);
+        });
+    }
+
+
+
     onToolbarCreated() {
         // Create a new toolbar group if it doesn't exist
         this._group = this.viewer.toolbar.getControl('AwesomeExtensionsToolbar');
@@ -52,24 +67,53 @@ class HandleSelectionExtension extends Autodesk.Viewing.Extension {
         let started = false;
 
         handleButton.onClick = (ev) => {
+            // Check if the panel is created or not
+            if (this._panel == null) {
+                this._panel = new ModelSummaryPanel(this.viewer, this.viewer.container, 'quantityChartPanel', 'Quantity Chart');
+            }
             // Get current selection
             const selection = this.viewer.getSelection();
             this.viewer.clearSelection();
-            // Anything selected?
+            this._panel.removeAllProperties();
+           
+           // Anything selected?
             if (selection.length > 0) {
+                
+                // Show/hide docking panel
+                this._panel.setVisible(!this._panel.isVisible());
+                // If panel is NOT visible, exit the function
+                if (!this._panel.isVisible())
+                    return;
+
                 let isolated = [];
-                // let redColor = new THREE.Vector4(1, 0, 0, 0.5);
                 // Iterate through the list of selected dbIds
                 selection.forEach((dbId) => {
                     // Get properties of each dbId
                     this.viewer.getProperties(dbId, (props) => {
-                        // Output properties to console
-                        console.log(props);
+                        props.properties.forEach((item) => {
+
+                            switch (item.displayName){
+                                case 'Width':
+                                    console.log(Math.round(item.displayValue));
+                                    this._panel.addProperty(item.displayName, Math.round(item.displayValue));
+                                    break;
+                                case 'Height':
+                                    console.log(Math.round(item.displayValue));
+                                    this._panel.addProperty(item.displayName, Math.round(item.displayValue));
+                                    break;
+                                case 'Area':
+                                    console.log(Math.round(item.displayValue));
+                                    this._panel.addProperty(item.displayName, `${Math.round(item.displayValue)} Sq.m.`);
+                                    break;
+                            }
+
+                        })
+                        
                         // Ask if want to isolate
                         if (selection) {
                             isolated.push(dbId);
                             this.viewer.isolate(isolated);
-                            // this.viewer.setThemingColor(dbId, redColor);
+                            
                         }
                     });
                 });
